c757ccdcf9ec43cfbb717e5a3b49257806000eba
Umut5446 <99072971@mydavinci.nl>
2022-09-15 14:19:28 +0200

    Eerste commit

---
+++ feestlunch.py
@@ -0,0 +1,13 @@
+croissants = 0.39
+stokbrood = 2.78
+kortingsbonnen = 0.50
+
+aantal_croissants = 17
+aantal_stokbrood = 2
+aantal_kortingsbonnen = 3
+
+print(aantal_croissants * croissants)
+print(aantal_stokbrood * stokbrood)
+print(aantal_kortingsbonnen * kortingsbonnen)
+
+print(aantal_croissants * croissants + aantal_stokbrood * stokbrood - aantal_kortingsbonnen * kortingsbonnen)
