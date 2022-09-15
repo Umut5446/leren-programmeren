8f78dab4d221a91faf3a48d46382c90beaa9002e
Umut5446 <99072971@mydavinci.nl>
2022-09-15 15:22:49 +0200

    Eerste commit

---
+++ speelhal.py
@@ -0,0 +1,9 @@
+toegangsticket = 7.45
+vip = 0.37
+aantal_minuut = 5
+aantal_personen = 4
+
+print(aantal_personen * toegangsticket)
+print(aantal_personen * 9 * vip)
+
+print(aantal_personen * toegangsticket + aantal_personen * vip * 9)
