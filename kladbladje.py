66aea9aa33147561c0238981e0a405ea1c1efafc
Umut5446 <99072971@mydavinci.nl>
2022-09-15 11:49:57 +0200

    Eerste commit

---
+++ kladbladje.py
@@ -0,0 +1,20 @@
+ #vraag de tijd in uren en minuten
+# en print: Dan duurt de dag nog x uur en y minuten
+
+vraag = "Hoe laat is het in uren?"
+uren_string = input(vraag)
+
+uren = int(uren_string)
+
+print(uren_string)
+
+#print(f"Dan duurt het nog {23 - uren} uur en {60 - minuten} minuten voordat de dag voorbij is.")
+
+vraag = "Hoe laat is het in minuten?"
+minuten_string = input(vraag)
+
+minuten = int(minuten_string)
+
+print(minuten_string)
+
+ 
