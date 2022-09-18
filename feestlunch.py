182deebafeefe51e1201ef0d43c5cb0f8023c9b1
Umut5446 <99072971@mydavinci.nl>
2022-09-18 21:42:20 +0200

    Eerste commit

--- feestlunch.py
+++ feestlunch.py
@@ -6,8 +6,13 @@ aantal_croissants = 17
 aantal_stokbrood = 2
 aantal_kortingsbonnen = 3
 
-print(aantal_croissants * croissants)
-print(aantal_stokbrood * stokbrood)
-print(aantal_kortingsbonnen * kortingsbonnen)
+totaal_prijs_croissants = aantal_croissants * croissants
+print("totaal bedrag croissantjes: ",totaal_prijs_croissants)
+totaal_prijs_stokbroden = aantal_stokbrood * stokbrood
+print("totaal bedrag stokbroden: ",totaal_prijs_stokbroden)
+totaal_prijs_kortingsbonnen = aantal_kortingsbonnen * kortingsbonnen
+print("totaal bedrag kortingsbonnen: ",totaal_prijs_kortingsbonnen)
 
-print(aantal_croissants * croissants + aantal_stokbrood * stokbrood - aantal_kortingsbonnen * kortingsbonnen)
+totaal_prijs = (aantal_croissants * croissants + aantal_stokbrood * stokbrood - aantal_kortingsbonnen * kortingsbonnen)
+
+print(f"De feestlunch kost je bij de bakker {totaal_prijs} euro voor de {aantal_croissants} croissantjes en de {aantal_stokbrood} stokbroden als de {aantal_kortingsbonnen} kortingsbonnen nog geldig zijn!")
--- speelhal.py
+++ speelhal.py
@@ -2,8 +2,10 @@ toegangsticket = 7.45
 vip = 0.37
 aantal_minuut = 5
 aantal_personen = 4
-
+aantal_minuten = 5 * 9
 print(aantal_personen * toegangsticket)
 print(aantal_personen * 9 * vip)
 
-print(aantal_personen * toegangsticket + aantal_personen * vip * 9)
+totaal_prijs = (aantal_personen * toegangsticket + aantal_personen * vip * 9)
+
+print(f"Dit geweldige dagje-uit met {aantal_personen} in de Speelhal met {aantal_minuten} minuten VR kost je maar {totaal_prijs} euro")
