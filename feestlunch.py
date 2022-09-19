fe8d956399c8fe7633ff65edc02b0d039ab66ead
Umut5446 <99072971@mydavinci.nl>
2022-09-19 13:50:23 +0200

    Eerste commit

--- feestlunch.py
+++ feestlunch.py
@@ -6,6 +6,19 @@ aantal_croissants = 17
 aantal_stokbrood = 2
 aantal_kortingsbonnen = 3
 
+croissants = float(input(f'hoeveel kost het croissants per stuk?')) 
+round(croissants,2)
+aantal_croissants = int(input(f'Hoeveel stuks croissants heeft u?'))
+
+stokbrood = float(input(f'hoeveel kost het stokbrood per stuk?'))
+round(stokbrood,2)
+aantal_stokbrood = int(input(f'Hoeveel stuks stokbrood heeft u?'))
+
+kortingabonnen = float(input(f'hoeveel kost het kortingsbonnen?'))
+round(kortingsbonnen,2)
+aantal_kortingsbonnen = int(input(f'Hoeveel stuks kortingsbonnen heeft u?'))
+
+
 totaal_prijs_croissants = aantal_croissants * croissants
 print("totaal bedrag croissantjes: ",totaal_prijs_croissants)
 totaal_prijs_stokbroden = aantal_stokbrood * stokbrood
