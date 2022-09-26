3f27aed2450f16f8f8dbde5ab5b7288f24de831a
Umut5446 <99072971@mydavinci.nl>
2022-09-26 09:57:11 +0200

    iput en if-statement

--- i-statement.py
+++ i-statement.py
@@ -1,15 +1,17 @@
+from re import A
+
+
 a = int(input('Geef het grootste getal.'))
 b= int(input('Geef het grootste getal.'))
 
 if a > b:
     Max = a
     print(f'a is het grootste getal.')
-    max = print(f'Het maximum is: gevolgd door de waarde van max.')
+    max = print(f'Het maximum is: {a}.')
 elif a < b:
     Min = b 
     print(f'a is het kleinste getal.')
-    min = print(f'Het minimum: is: gevolgd door de waarde van min.')
+    min = print(f'Het minimum: is: {b}.')
 else:
-    a = b
     print(f'a en b zijn even groot.')
  
