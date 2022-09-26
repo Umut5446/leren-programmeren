704481b4f04675859a92459be6c332e5543db1fe
Umut5446 <99072971@mydavinci.nl>
2022-09-26 09:44:08 +0200

    input en if-statement

--- i-statement.py
+++ i-statement.py
@@ -1,5 +1,15 @@
-a = int(input('Geef het grootste getal'))
-b= int(input('Geef het grootste getal'))
+a = int(input('Geef het grootste getal.'))
+b= int(input('Geef het grootste getal.'))
 
 if a > b:
     Max = a
+    print(f'a is het grootste getal.')
+    max = print(f'Het maximum is: gevolgd door de waarde van max.')
+elif a < b:
+    Min = b 
+    print(f'a is het kleinste getal.')
+    min = print(f'Het minimum: is: gevolgd door de waarde van min.')
+else:
+    a = b
+    print(f'a en b zijn even groot.')
+ 
