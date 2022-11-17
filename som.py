1be7759b280249f89ee9c3cbe85b01f4de5d1704
Umut5446 <99072971@mydavinci.nl>
2022-11-17 13:18:06 +0100

    while quit.py

---
+++ som.py
@@ -0,0 +1,3 @@
+for x in range(50,1000):
+    x += 1
+    print(f'{x} + {x}= {x + x}')
--- while_quit.py
+++ while_quit.py
@@ -1,9 +1,3 @@
-# x = quit
-# while x == quit:
-#     y = input('Typ iets in!')
-#     print(y)
-#     print('Als je wilt stoppen typ quit in!')
-    
 teller = 1
 
 x = input('Typ iets in!') 
