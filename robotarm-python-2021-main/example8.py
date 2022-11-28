d7c11542c65ca639844f269f9b4d1de0d01edf93
Umut5446 <99072971@mydavinci.nl>
2022-11-28 23:16:34 +0100

    example8.py

--- robotarm-python-2021-main/example8.py
+++ robotarm-python-2021-main/example8.py
@@ -4,6 +4,14 @@ robotArm = RobotArm('exercise 8')
 robotArm.speed = 2 
 
 # Jouw python instructies zet je vanaf hier:
+robotArm.moveRight()
+for x in range(7):
+    robotArm.grab()
+    for x in range(8):
+        robotArm.moveRight()
+    robotArm.drop()
+    for y in range(8):
+        robotArm.moveLeft()
  
 
 # Na jouw code wachten tot het sluiten van de window:
