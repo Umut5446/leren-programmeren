1eaee25e9b6f2ba0aed925428e01ddae1d0d8c9c
Umut5446 <99072971@mydavinci.nl>
2022-12-14 23:35:25 +0100

    coins.py

--- robotarm-python-2021-main/example10.py
+++ robotarm-python-2021-main/example10.py
@@ -4,17 +4,14 @@ robotArm = RobotArm('exercise 10')
 robotArm.speed = 2
 
 # Jouw python instructies zet je vanaf hier:
-teller = 9
- 
-for x in range(5):
-  robotArm.grab()
-  for x in range(9):
-    robotArm.moveRight()
-  robotArm.drop()
-  teller -= 2
-  for x in range(teller):
-    robotArm.moveLeft()
-  teller -= 2
+for x in range(4):
+    robotArm.grab()
+    for x in range(5): robotArm.moveRight()
+    robotArm.drop()
+    for x in range(4): robotArm.moveLeft()
+robotArm.grab()
+for x in range(5): robotArm.moveRight()
+robotArm.drop()
 
 # robotArm.grab()
 # for x in range(7):
