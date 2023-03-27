0141ef02e3ebd9b1a7a1974df9eb693dda6fd82f
Umut5446 <99072971@mydavinci.nl>
2023-03-27 09:16:40 +0200

    wat_maken.py

--- module4/Deck_van_kaarten.py
+++ module4/Deck_van_kaarten.py
@@ -1,6 +1,10 @@
 kleuren = ('harten', 'klaveren', 'schoppen', 'ruiten')
-soorten = ('2', '3', '4', '5', '6', '7', '8', '9', '10')
+soorten = ('boer', 'vrouw', 'heer', 'aas','2', '3', '4', '5', '6', '7', '8', '9', '10',  )
+ 
+kaarten = ['joker1', 'joker2']
+for x in range(12):
+    for y in range(4):
+        kaarten.append(kleuren[y] + ' ' + soorten[x])
+print(kaarten)
 
-kaarten = ['joker1' 'joker2']
-print(kleuren[0])
-print(soorten[0])
+ 
