6bcb471fb1e6b7086c4462936d30b5190a8e4b53
Umut5446 <99072971@mydavinci.nl>
2023-04-17 09:34:24 +0200

    wat_maken.py

--- module4/Deck_van_kaarten.py
+++ module4/Deck_van_kaarten.py
@@ -1,3 +1,4 @@
+import random
 kleuren = ('harten', 'klaveren', 'schoppen', 'ruiten')
 soorten = ('boer', 'vrouw', 'heer', 'aas','2', '3', '4', '5', '6', '7', '8', '9', '10',)
  
@@ -5,6 +6,9 @@ kaarten = ['joker1', 'joker2']
 for x in range(13):
     for y in range(4):
         kaarten.append(kleuren[y] + ' ' + soorten[x])
+        # random.shuffle(kleuren[y] + ' ' + soorten[x]) 
 print(kaarten)
 
-('kaart1:' (kleuren[y] + ' ' + soorten[x])) 
+for x in range(0):
+    kaarten.append(kleuren[y] + ' ' + soorten[x])
+    print(kaarten)
